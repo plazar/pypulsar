826d0f8cf39a76825a966cec3563004f5dc5ddb2
plazar <plazar@f9d5805e-cc80-4840-9830-5177e7b0ec9f>
2009-11-09 05:39:07 +0000

    Added new functions to 'astro_utils/calendar.py'.
    git-svn-id: file://localhost/data/alfa/plazar/svn@94 f9d5805e-cc80-4840-9830-5177e7b0ec9f

--- trunk/python/astro_utils/calendar.py
+++ trunk/python/astro_utils/calendar.py
@@ -112,6 +112,90 @@ def gregorian_to_JD(year, month, day):
     return date_to_JD(year, month, day, gregorian=True)
 
 
+def JD_to_date(JD):
+    """Convert Julian Day (JD) to a date.
+        
+        Input:
+            JD: Julian day
+
+        (Follow Jean Meeus' Astronomical Algorithms, 2nd Ed., Ch. 7)
+    """
+    JD = np.atleast_1d(JD)
+    
+    if np.any(JD<0.0):
+        raise ValueError("This function does not apply for JD < 0.")
+
+    JD += 0.5
+
+    # Z is integer part of JD
+    Z = np.floor(JD)
+    # F is fractional part of JD
+    F = np.mod(JD, 1)
+
+    A = np.copy(Z)
+    alpha = np.floor((Z-1867216.25)/36524.25)
+    A[Z>=2299161] = Z + 1 + alpha - np.floor(0.25*alpha)
+
+    B = A + 1524
+    C = np.floor((B-122.1)/365.25)
+    D = np.floor(365.25*C)
+    E = np.floor((B-D)/30.6001)
+
+    day = B - D - np.floor(30.6001*E) + F
+    month = E - 1
+    month[E==14.0 or E==15.0] = E - 13
+    year = C - 4716
+    year[month==1.0 or month==2.0] = C - 4715
+
+    return (year.astype('int').squeeze(), month.astype('int').squeeze(), \
+                day.squeeze())
+
+
+def MJD_to_date(MJD):
+    """Convert Modified Julian Day (MJD) to a date.
+        
+        Input:
+            MJD: Modified Julian day
+
+        (Follow Jean Meeus' Astronomical Algorithms, 2nd Ed., Ch. 7)
+    """
+    JD = MJD_to_JD(MJD)
+    return JD_to_date(JD)
+
+
+def is_leap_year(year, gregorian=True):
+    """Return True if year is a leap year.
+        
+        Inputs:
+            year: integer
+            gregorian:  - True for Gregorian calendar (Default)
+                        - False for Julian calendar
+    """
+    year = np.atleast_1d(year)
+    leap = (year%4)==0
+    if gregorian:
+        leap = np.bitwise_and(leap, np.bitwise_or((year%400)==0, (year%100)!=0))
+    return leap.squeeze()
+
+
+def is_gregorian_leap_year(year):
+    """Return True if year is a leap year.
+        
+        Inputs:
+            year: integer
+    """
+    return is_leap_year(year, gregorian=True)
+
+
+def is_julian_leap_year(year):
+    """Return True if year is a leap year.
+        
+        Inputs:
+            year: integer
+    """
+    return is_leap_year(year, gregorian=False)
+
+
 def month_to_num(month):
     """Return month number given the month name, a string.
     """
