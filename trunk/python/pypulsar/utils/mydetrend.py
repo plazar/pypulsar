956d74ba69dcf50ce1265e92f5177c79b9815aaf
plazar <plazar@f9d5805e-cc80-4840-9830-5177e7b0ec9f>
2011-06-15 15:17:18 +0000

    Modified 'mydetrend.py' to accept both x and y data.
    git-svn-id: file://localhost/data/alfa/plazar/svn@141 f9d5805e-cc80-4840-9830-5177e7b0ec9f

--- trunk/python/pypulsar/utils/mydetrend.py
+++ trunk/python/pypulsar/utils/mydetrend.py
@@ -10,33 +10,39 @@ Patrick Lazarus, Nov. 13, 2010
 import numpy as np
 import scipy.linalg
 
-def detrend(data, order=1, mask=None):
+import matplotlib.pyplot as plt
+
+
+def detrend(ydata, xdata=None, mask=None, order=1):
     """Detrend 'data' using a polynomial of given order.
     
         Inputs:
-            data: a 1D array to be detrended.
-            order: Order of polynomial to use (Default: 1)
+            ydata: A 1D array to be detrended.
+            xdata: A 1D array of x-values to use
+                    (Optional. Default: Use indices at xdata).
             mask: A 1D array of booleans to used to omit
                     values in data when detrending.
                     True means omit value.
                     (Default: Mask nothing)
+            order: Order of polynomial to use (Default: 1)
 
         Output:
             detrended: a 1D array.
     """
-    indices = np.arange(data.size)
+    if xdata is None:
+        xdata = np.arange(ydata.size)
     powers = np.arange(order+1)
 
-    A = np.repeat(np.arange(indices.size), order+1)
-    A.shape = (indices.size, order+1)
+    A = np.repeat(xdata, order+1)
+    A.shape = (xdata.size, order+1)
     A = A**powers
 
     if mask is None:
-        unmasked = np.ones(data.size, dtype='bool')
+        unmasked = np.ones(ydata.size, dtype='bool')
     else:
         unmasked = np.bitwise_not(mask) 
-    x, resids, rank, s = scipy.linalg.lstsq(A[unmasked], data[unmasked])
-    detrended =  data - np.dot(A, x)
+    x, resids, rank, s = scipy.linalg.lstsq(A[unmasked], ydata[unmasked])
+    detrended =  ydata - np.dot(A, x)
     return detrended
 
 
