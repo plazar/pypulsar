0f019523d958f79392ffac3eee8ce06539b92429
Patrick Lazarus <plazarus@mpifr-bonn.mpg.de>
2014-02-07 11:07:53 +0100

    Rewrite function used to calculate SNR. Also, calculate mean flux-density if SEFD is provided on cmdline.

--- bin/pfd_snr.py
+++ bin/pfd_snr.py
@@ -7,6 +7,7 @@ Manually determine on-pulse region for the profile in a pfd file
 """
 
 import sys
+import argparse
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,16 +20,25 @@ import psr_utils
 from pypulsar.utils.astro import protractor
 debug = 1
 
+
+class OnPulseError(Exception):
+    pass
+
 class Observation:
     """ Observation object
     """
-    def __init__(self, pfdfn):
-        """
-        Read pfd file and return arrays of snr, ra and dec
+    def __init__(self, pfdfn, sefd=None):
+        """Return an observation object for the given pfd file.
     
-        Input: pfdfn - a pfd filename
+        Inputs: 
+            pfdfn: a pfd filename
+            sefd: the system-equivalent flux density of the observation (in Jy)
+
+        Output:
+            obs: The Observation object            
         """
         self.fn = pfdfn
+        self.sefd = sefd
         self.p = prepfold.pfd(pfdfn)
         # RA in arcmin
         self.ra = protractor.convert(self.p.rastr, 'hmsstr', 'deg')*60
@@ -39,11 +49,9 @@ class Observation:
         
         prof = self.p.bestprof.profile
         imax = np.argmax(prof)
-        nrot = (imax-len(prof)/2) % len(prof)
-        print "Profile maximum at bin %d. Rotating by %d bins." % (imax, nrot)
-        self.prof = psr_utils.rotate(prof, nrot)
-        
-        self.get_snr()
+        self.nrot = (imax-len(prof)/2) % len(prof)
+        print "Profile maximum at bin %d. Rotating by %d bins." % (imax, self.nrot)
+        self.prof = psr_utils.rotate(prof, self.nrot)
 
     def get_snr(self):
         """
@@ -53,7 +61,7 @@ class Observation:
         """
         self.fig = plt.figure()
         self.ax = plt.gca()
-        plt.plot(self.prof, 'k-')
+        plt.plot(self.prof, 'k-', drawstyle='steps-post')
         self.cid_keypress = self.fig.canvas.mpl_connect('key_press_event', \
                                                             self.keypress)
         self.cid_mouseclick = self.fig.canvas.mpl_connect('button_press_event', \
@@ -119,33 +127,57 @@ class Observation:
             xmax = len(self.prof)-1
         xmin = int(np.round(xmin))
         xmax = int(np.round(xmax))
+        self.onpulse_range = (xmin, xmax)
+        print self.onpulse_range
+        self.calc_snr()
+        self.disconnect_triggers()
 
+    def calc_snr(self):
+        if self.onpulse_range is None:
+            raise OnPulseError("No on-pulse region selected!")
+        # Correct standard deviation for correlations between bins
+        nbin_eff = self.p.bestprof.proflen*self.p.DOF_corr()
+        std = self.p.bestprof.data_std*np.sqrt(self.p.bestprof.N/nbin_eff)
+       
+        xmin, xmax = self.onpulse_range
         # Calculate S/N using eq. 7.1 from Lorimer and Kramer
         offpulse = np.concatenate((self.prof[:xmin], \
                                     self.prof[xmax:]))
-        onpulse = self.prof[xmin:xmax]
         mean = offpulse.mean()
-        std = offpulse.std()
-        self.oldsnr = ((self.prof - mean) / std).max()
-        weq = trapz((onpulse-mean)/std)/(np.max((onpulse-mean)/std))
+        scaled = self.prof-mean
+        print xmin, xmax
+        print scaled[xmin:xmax]
+        #plt.figure()
+        #plt.plot(scaled, drawstyle='steps-post')
+        #plt.show()
+        area = np.sum(scaled[xmin:xmax]) 
+        profmax = np.max(scaled[xmin:xmax])
+        weq = area/profmax
         self.width = xmax-xmin
-        self.snr = np.sum(self.prof-mean)/std/np.sqrt(weq)
+        self.snr = area/std/np.sqrt(weq)
+        print "Width selected: %d bins, %f phase" % \
+                (self.width, self.width/float(len(self.prof)))
         if debug:
-            print "Width selected (bins):", self.width
             print "Equivalent width (bins):", weq
-            print "Integral under the normalised on-pulse region:", \
-                    trapz((onpulse-mean)/std)
-            print "SNR:", self.snr
+            print "Std-dev corrected for correlations between phase bins:", std
+            print "Off-pulse mean:", mean
+            print "Integral under the mean-subtracted on-pulse region:", \
+                    area
+        print "SNR:", self.snr
 
+        if self.sefd is not None:
+            npol = 2  # prepfold files only contain total-intensity
+                      # (i.e. both polarisations summed)
+            bw = self.p.chan_wid*self.p.numchan
+            smean = self.snr*self.sefd/np.sqrt(npol*self.p.T*bw)*np.sqrt(weq/(len(self.prof)-weq))
+            print "Mean flux density (mJy):", smean
+
+    def disconnect_triggers(self):
         # disconnect mousemove, mouserelease and reset state
         self.eventpress = None
         self.mousepressed = False
         self.fig.canvas.mpl_disconnect(self.cid_mousemove)
         self.fig.canvas.mpl_disconnect(self.cid_mouserelease)
-
-        # Display width and snr
-        print "SNR:", self.snr
-        print "Width (bins):", self.width
 
     def keypress(self, event):
         if event.key == 'enter' and self.snr is not None and \
@@ -154,12 +186,29 @@ class Observation:
 
 
 def main():
-    pfdfns = sys.argv[1:]
-    for pfdfn in pfdfns:
+    for pfdfn in args.files:
         print pfdfn
-        obs = Observation(pfdfn)        
+        obs = Observation(pfdfn, sefd=args.sefd)
+        if args.on_pulse is None:
+            obs.get_snr()
+        else:
+            print args.on_pulse
+            start_bin = int(args.on_pulse[0]*len(obs.prof)-obs.nrot + 0.5) % len(obs.prof)
+            end_bin = int(args.on_pulse[1]*len(obs.prof)-obs.nrot+0.5) % len(obs.prof)
+            obs.onpulse_range = (start_bin, end_bin)
+            print "Rotated on-pulse range (bins): %d - %d" % (start_bin, end_bin)
+            obs.calc_snr()
         print "%s \tSNR: %s" % (obs.fn, obs.snr)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Calculate SNR from .pfd files.")
+    parser.add_argument('files', nargs='*', \
+                        help="Files to find SNR for.")
+    parser.add_argument('--on-pulse', dest='on_pulse', nargs=2, type=float, \
+                        help="On-pulse region. Two values should be provided, " \
+                            "the starting phase and the ending phase.")
+    parser.add_argument('--sefd', dest='sefd', type=float, \
+                        help="The SEFD of the observing system.")
+    args = parser.parse_args()
     main()
